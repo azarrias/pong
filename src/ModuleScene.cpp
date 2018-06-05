5e14065b34b72019a62e7a151e5db778e3774359
azarrias <adolfo.zarrias@gmail.com>
2018-06-05 21:13:19 +0200

    Setup ball spawning angle between 15 and 45 deg

--- src/ModuleScene.cpp
+++ src/ModuleScene.cpp
@@ -5,12 +5,15 @@
  *      Author: adolfo
  */
 
+#include <cmath>
 #include "GameManager.h"
 #include "GameObject.h"
 #include "Globals.h"
 #include "ModuleEntityManager.h"
 #include "ModuleScene.h"
 #include "Random.h"
+
+#define PI 3.14159265
 
 ModuleScene::ModuleScene()
 {}
@@ -55,7 +58,30 @@ bool ModuleScene::Start()
 {
 	game->mEntities->mBall->SetPos(SCREEN_WIDTH / 2 - BALL_WIDTH / 2,
 		SCREEN_HEIGHT / 2 - BALL_HEIGHT / 2);
-	game->mEntities->mBall->mVelocity.Set(Random::GetRandom(-2, 2), Random::GetRandom(-2, 2));
+
+	float startPosX = 0.0f;
+	float startPosY = 0.0f;
+
+	// Start ball with angle between 15 and 45 degrees
+	while(startPosX + startPosY < 0.1f)
+	{
+		startPosX = Random::GetRandom(0.0f, 1.0f);
+		startPosY = Random::GetRandom((float)tan(15 * PI / 180.0) * startPosX, startPosX);
+	}
+
+	// Then randomize sign
+	if(Random::GetRandom(0, 1))
+	{
+		startPosX = - startPosX;
+	}
+	if(Random::GetRandom(0, 1))
+	{
+		startPosY = - startPosY;
+	}
+
+	game->mEntities->mBall->mVelocity.Set(startPosX, startPosY);
+	game->mEntities->mBall->mVelocity.Normalize();
+	game->mEntities->mBall->mVelocity *= 10;
 
     return true;
 }
